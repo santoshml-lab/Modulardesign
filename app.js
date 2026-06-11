edd536931bf7eaa37116b54862ef9ab0b09d9ec8
santoshml-lab <santoshkrsbg36@gmail.com>
2026-06-11 12:05:31 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+function openLink(url){
+window.open(url, "_blank");
+}
+
+function startLearning(){
+alert("Start Learning Clicked 🚀");
+}
+
+function resume(){
+alert("Resuming last topic 📚");
+}
+
+// Future: API integration
