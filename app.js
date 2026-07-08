e453d3428f61388e278dece6acb58e523657674c
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-08 15:15:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 function openLink(url){
 window.open(url, "_blank");
 }
+const aiTips = [
+
+"📘 Revise Biology for 30 minutes.",
+
+"🧮 Solve 15 Algebra questions today.",
+
+"⚛ Practice 10 Physics numericals.",
+
+"🧪 Revise Organic Chemistry reactions.",
+
+"📄 Upload one PDF and generate AI Notes.",
+
+"🎯 Attempt one Mock Test today.",
+
+"🔥 Keep your study streak alive!"
+
+];
+
+function nextTip(){
+
+const random =
+Math.floor(Math.random()*aiTips.length);
+
+document.getElementById("aiMessage").innerHTML =
+aiTips[random];
+
+}
